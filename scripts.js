fa94185db1de7a8906a9f8f04164d2ae0140507a
brunoamaia <brunoamaia94@gmail.com>
2021-03-02 11:23:34 -0300

    update controls and speed

--- scripts.js
+++ scripts.js
@@ -52,8 +52,8 @@ function startGame() {
 
   if (direction == 'right') {snakeX += box}
   if (direction == 'left') {snakeX -= box}
-  if (direction == 'up') {snakeY += box}
-  if (direction == 'down') {snakeY -= box}
+  if (direction == 'up') {snakeY -= box}
+  if (direction == 'down') {snakeY += box}
   
   if (snakeX === food.x && snakeY === food.y) {
     food.x = Math.floor(Math.random()*15+1) * box
@@ -72,10 +72,10 @@ function startGame() {
 document.addEventListener('keydown', updateDirection)
 function updateDirection(event) {
   if(event.keyCode == 37 && direction != "right") { direction = "left"}
-  if(event.keyCode == 38 && direction != "up") { direction = "down"}
+  if(event.keyCode == 38 && direction != "down") { direction = "up"}
   if(event.keyCode == 39 && direction != "left") { direction = "right"}
-  if(event.keyCode == 40 && direction != "down") { direction = "up"}
+  if(event.keyCode == 40 && direction != "up") { direction = "down"}
 }
 
 
-let game = setInterval(startGame, 200)
+let game = setInterval(startGame, 100)
