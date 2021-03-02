1b5361ab05defa8be726f21be71244f06b20f5b1
brunoamaia <brunoamaia94@gmail.com>
2021-03-02 10:00:31 -0300

    create snake and move for right

--- scripts.js
+++ scripts.js
@@ -7,4 +7,39 @@ function criarBG() {
   context.fillRect(0, 0, 16*box, 16*box)
 }
 
-criarBG();
+let snake = []
+snake[0] = {
+  x: 8*box,
+  y: 8*box
+}
+function createSnake() {
+  for(i=0; i<snake.length; i++) {
+    context.fillStyle ="green"
+    context.fillRect(snake[i].x,snake[i].y, box, box)
+  }
+}
+
+
+let direction = 'right'
+function startGame() {
+  criarBG()
+  createSnake()
+  let snakeX = snake[0].x
+  let snakeY = snake[0].y
+
+  if (direction == 'right') {snakeX += box}
+  if (direction == 'left') {snakeX -= box}
+  if (direction == 'up') {snakeY += box}
+  if (direction == 'down') {snakeY -= box}
+  snake.pop()
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY
+  }
+
+  snake.unshift(newHead)
+
+}
+
+let game = setInterval(startGame, 100)
