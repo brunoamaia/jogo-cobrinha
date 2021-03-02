35f859e6cb598d26790322317a066ad51db3177f
brunoamaia <brunoamaia94@gmail.com>
2021-03-02 10:30:04 -0300

    change direction and  teleport from end of screen

--- scripts.js
+++ scripts.js
@@ -20,8 +20,14 @@ function createSnake() {
 }
 
 
-let direction = 'right'
+let direction = "right"
 function startGame() {
+
+  if (snake[0].x > 15*box && direction == "right") {snake[0].x = 0}
+  if (snake[0].x < 0 && direction == "left") {snake[0].x = 15*box}
+  if (snake[0].y > 15*box && direction == "up") {snake[0].y = 0}
+  if (snake[0].y < 0 && direction == "down") {snake[0].y = 15*box}
+
   criarBG()
   createSnake()
   let snakeX = snake[0].x
@@ -39,7 +45,15 @@ function startGame() {
   }
 
   snake.unshift(newHead)
-
 }
 
-let game = setInterval(startGame, 100)
+document.addEventListener('keydown', updateDirection)
+function updateDirection(event) {
+  if(event.keyCode == 37 && direction != "right") { direction = "left"}
+  if(event.keyCode == 38 && direction != "up") { direction = "down"}
+  if(event.keyCode == 39 && direction != "left") { direction = "right"}
+  if(event.keyCode == 40 && direction != "down") { direction = "up"}
+}
+
+
+let game = setInterval(startGame, 200)
